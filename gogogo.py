c00b03622b13dcd29c0257c47c2964f68fd9502b
eknhakcl <ekinhakcil19@gmail.com>
2025-10-06 17:36:42 +0300

    trial

---
+++ gogogo.py
@@ -0,0 +1,12 @@
+# Enter 3 numbers and print the largest one
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+
+if a > b and a > c:
+    print("The largest number is:", a)
+elif b > a and b > c:
+    print("The largest number is:", b)
+else:
+    print("The largest number is:", c)
+
