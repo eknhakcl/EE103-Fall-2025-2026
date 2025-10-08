832e1ba6e298cc460bbb8d46be9714fd9b1a3757
eknhakcl <ekinhakcil19@gmail.com>
2025-10-08 23:00:34 +0300

    ee103

---
+++ mysum.py
@@ -0,0 +1,4 @@
+mysum = 0
+for i in range(3,6):
+    mysum += i  
+print(mysum)
---
+++ smt.py
@@ -0,0 +1,10 @@
+n= 0 
+direction = input("right or left: ")
+while direction == "right":
+    n = n+1
+    if n<2 :
+        input("right or left: ")
+    else:
+        print(":(")
+else:
+    print("you got out")
